dec4735501dab23aa6caf0acde458a223bfa1f01
Phạm Thanh Danh <47974698+thanhdanh27600@users.noreply.github.com>
2021-08-02 17:39:32 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/iot_final/MainActivity.java
+++ app/src/main/java/com/example/iot_final/MainActivity.java
@@ -13,7 +13,6 @@ import android.hardware.usb.UsbManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
